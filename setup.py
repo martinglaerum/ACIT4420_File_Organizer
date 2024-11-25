485026b70a5f4fc0a04186b7ad857c6dbc4bf0c7
Martin Solevåg Glærum <74411819+martinglaerum@users.noreply.github.com>
2024-11-25 11:00:17 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ACIT_Exam_Task2",  # The package name
+    version="0.1",
+    packages=find_packages(),  # Automatically find all packages in your project
+    include_package_data=True,  # Include non-Python files specified in MANIFEST.in (if any)
+    description="A tool for sorting files into folders mathing their file extension",
+    author="Martin Solevåg Glærum",
+    author_email="magla7524@oslomet.no",
+    entry_points={
+        'console_scripts': [
+            'file_organizer=file_organizer.main:main',  # Points directly to the main function in main.py
+        ],
+    },
+)
