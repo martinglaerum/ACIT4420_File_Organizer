c95d02babd0a46e1e58a22d86d905077b436cad5
Martin Solevåg Glærum <74411819+martinglaerum@users.noreply.github.com>
2024-11-25 12:05:26 +0100

    Update main.py

--- file_organizer/main.py
+++ file_organizer/main.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+        # Ensure the parent directory is in the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 import find_files_and_folder
 import move_files
 
