6fbee6687645a0485084734dfae61cb8989bee45
Martin Solevåg Glærum <74411819+martinglaerum@users.noreply.github.com>
2024-11-28 13:55:12 +0100

    Update logger.py

--- file_organizer/logger.py
+++ file_organizer/logger.py
@@ -6,7 +6,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def log_info(file, folder):
     file_path = os.path.join(BASE_DIR, "log.txt") # Specifies the path to the log file
-    print(file, folder, file_path)
         # Opens the log file and stores info about each file that was moved
     with open(file_path, "at") as f:
         f.write(f"Time: {datetime.datetime.now().replace(microsecond=0)}, The file {file} was moved to the folder {folder}\n")
